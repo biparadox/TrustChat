0a38bd858c23907767d128a4403e1a2b29fb2053
Secure Cube <algorist@bjut.edu.cn>
2016-05-31 04:52:53 +0800

    add friend list

---
+++ MsgHub/friend_list.h
@@ -0,0 +1,8 @@
+#ifndef FRIEND_LIST_H
+#define FRIEND_LIST_H
+
+// plugin's init func and kickstart func
+int friend_list_init(void * sub_proc,void * para);
+int friend_list_start(void * sub_proc,void * para);
+
+#endif
