0a3334ac7ef84e7118b6eb9ba3f66d21e68b8028
Secure Cube <algorist@bjut.edu.cn>
2016-05-31 09:16:42 +0800

    add key_storagefunc

---
+++ MsgServer/key_storage_func.c
@@ -0,0 +1,96 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <limits.h>
+#include <dirent.h>
+#include <sys/time.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <openssl/rsa.h>
+#include <openssl/evp.h>
+
+#include "../include/data_type.h"
+#include "../include/struct_deal.h"
+#include "../include/crypto_func.h"
+#include "../include/extern_struct.h"
+#include "../include/extern_defno.h"
+#include "../include/message_struct.h"
+#include "../include/logic_baselib.h"
+#include "../include/sec_entity.h"
+#include "../include/main_proc_init.h"
+#include "../include/valuename.h"
+#include "../include/expand_define.h"
+
+#include "../cloud_config.h"
+#include "main_proc_func.h"
+
+int key_storage_init(void * sub_proc,void * para)
+{
+	int ret;
+	// add youself's plugin init func here
+	return 0;
+}
+
+int key_storage_start(void * sub_proc,void * para)
+{
+	int ret;
+	void * recv_msg;
+	int i;
+	const char * type;
+
+
+	for(i=0;i<3000*1000;i++)
+	{
+		usleep(time_val.tv_usec);
+		ret=sec_subject_recvmsg(sub_proc,&recv_msg);
+		if(ret<0)
+			continue;
+		if(recv_msg==NULL)
+			continue;
+		type=message_get_recordtype(recv_msg);
+		if(type==NULL)
+		{
+			printf("message format error!\n");
+			continue;
+		}
+		if(!find_record_type(type))
+		{
+			printf("message format is not registered!\n");
+			continue;
+		}
+		if(strncmp(type,"NKLD",4)==0)
+			proc_key_storage(sub_proc,recv_msg);
+	}
+
+	return 0;
+};
+
+int proc_key_storage(void * sub_proc,void * message)
+{
+	const char * type;
+	int i;
+	int ret;
+	printf("begin proc echo \n");
+	struct message_box * msg_box=message;
+
+	void * record;
+	
+	i=0;
+
+	ret=message_get_record(message,&record,i++);
+	if(ret<0)
+		return ret;
+	while(record!=NULL)
+	{
+		AddPolicy(record,"NKLD");
+		ret=message_get_record(message,&record,i++);
+		if(ret<0)
+			return ret;
+	}
+	ExportPolicy("NKLD");
+	return ret;
+}
