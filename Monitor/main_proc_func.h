abfe0e419d0493813a6952d435ef400965cc2df9
Secure Cube <algorist@bjut.edu.cn>
2016-07-08 18:53:59 +0800

    add Monitor function

---
+++ Monitor/main_proc_func.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_PROC_FUNC_H
+#define MAIN_PROC_FUNC_H
+//#include "echo_plugin_func.h"
+#include "session_msg.h"
+#include "general_lib_init.h"
+#include "../include/message_struct.h"
+#include "../include/message_struct_desc.h"
+#include "user_info.h"
+#include "echo_plugin_func.h"
+#include "websocket_func.h"
+
+int Monitor_init();
+
+
+#endif
---
+++ Monitor/proc_config.h
@@ -0,0 +1,26 @@
+#ifndef PROC_CONFIG_H
+#define PROC_CONFIG_H
+
+
+static char * main_proc_name="Monitor";
+static int  (*main_proc_initfunc)()=&Monitor_init;
+
+static PROCDB_INIT procdb_init_list[]=
+{
+	{"EEIE",NULL,&expand_extra_info_desc,NULL},
+	{"USNE",NULL,&user_name_expand_desc,NULL},
+	{"U2AL",&null_init_func,&user_addr_list_desc,&general_lib_ops},
+	{"MSGD",&general_uuid_lib_init,&session_msg_desc,&general_lib_ops},
+	{"UL_I",&general_lib_init,&user_info_list_desc,&general_lib_ops},
+	{"LOGI",NULL,&login_info_desc,NULL},
+	{"RETC",NULL,&connect_return_desc,NULL},
+	{NULL,NULL,NULL,NULL}
+};
+
+static PROC_INIT proc_init_list[]=
+{
+	{"ws_port",PROC_TYPE_MONITOR,&websocket_port_init,&websocket_port_start},
+	{NULL,0,NULL,NULL}
+};
+
+#endif // PROC_CONFIG_H
